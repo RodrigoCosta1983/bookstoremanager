53e6bbec60955c680305216bd8b610dffa008435
rodrigocosta1983 <rodrigo25.inf@outlook.com>
2022-11-11 06:02:17 -0300

    First UPDT

---
+++ bookstoremanager/src/test/java/com/rodrigocosta/bookstoremanager/BookstoremanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigocosta.bookstoremanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoremanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
